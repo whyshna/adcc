3b6d323cfd5876cb4884b562a04f3ac254cc20a4
Jonas Leitner <jonas.leitner1@gmx.de>
2026-01-10 15:13:37 +0100

    add triples symmetry factory

--- libadcc_src/make_symmetry.hh
+++ libadcc_src/make_symmetry.hh
@@ -100,5 +100,10 @@ std::shared_ptr<Symmetry> make_symmetry_operator(
 std::shared_ptr<Symmetry> make_symmetry_operator_basis(
       std::shared_ptr<const MoSpaces> mospaces_ptr, size_t n_bas, bool symmetric);
 
+/** Return the symmetry object for an 6 dimensional triples amplitude.
+*/
+std::shared_ptr<Symmetry> make_symmetry_triples(
+       std::shared_ptr<const MoSpaces> mospaces_ptr, const std::string& space);
+
 ///@}
 }  // namespace libadcc
